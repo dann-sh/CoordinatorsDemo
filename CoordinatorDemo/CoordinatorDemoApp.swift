10e4f45dfcb3a798c584cae888f1f518e03f9499
Daniel Serrano <dann.sh512@gmail.com>
2026-05-28 18:40:44 -0600

    Initial Commit

---
+++ CoordinatorDemo/CoordinatorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorDemoApp.swift
+//  CoordinatorDemo
+//
+//  Created by Daniel Serrano on 28/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoordinatorDemoTests/CoordinatorDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorDemoTests.swift
+//  CoordinatorDemoTests
+//
+//  Created by Daniel Serrano on 28/05/26.
+//
+
+import Testing
+@testable import CoordinatorDemo
+
+struct CoordinatorDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
